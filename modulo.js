82fe37a37c5306a5898f4555bf7b9a7886b88a09
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-29 10:22:27 +0100

    modulo

---
+++ modulo.js
@@ -0,0 +1,6 @@
+import algo from './palindromo.js';
+import { suma, multiplicacion } from './palindromo.js'
+
+algo(5, 1);
+suma(2, 3);
+multiplicacion(4, 2);
---
+++ palindromo.js
@@ -0,0 +1,9 @@
+export function suma(a, b){
+    console.log(a+b)
+}
+export function multiplicacion(a, b){
+    console.log(a* b)
+}
+export default function resta(a, b){
+    console.log("la resta es",a-b)
+}
