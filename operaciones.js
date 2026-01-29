0d4b0711166be62f85b6544edd70b12df80ec4ac
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-29 13:00:27 +0100

    funcion dentro de otra funcion

--- operaciones.js
+++ operaciones.js
@@ -4,4 +4,14 @@ export function sumar(e){
     const inputB=parseInt(e.target.B.value);
     console.log(inputA+inputB)
 
+    // nosotros podemos llamara cualquier funcion dentro de otra funcion, siempre y cuando esa otra funcion este siendo importada en el fichero de la funcion padre o que la otra funcion este declarada dentro del mismo fichero
+    imp_console()
+
+}
+
+
+
+
+function imp_console(){
+    console.log("esto es una funcion dentro de otra funcion")
 }
