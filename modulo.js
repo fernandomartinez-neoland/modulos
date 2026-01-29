42ffcc1336b0278fbfd656bf6d1ac6828f2f0d72
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-01-29 10:54:49 +0100

    palindromos

--- modulo.js
+++ modulo.js
@@ -1,6 +1,7 @@
 import algo from './palindromo.js';
-import { suma, multiplicacion } from './palindromo.js'
+import { suma, multiplicacion, palindromo } from './palindromo.js'
 
 algo(5, 1);
 suma(2, 3);
 multiplicacion(4, 2);
+palindromo("ama")
--- palindromo.js
+++ palindromo.js
@@ -1,9 +1,35 @@
-export function suma(a, b){
-    console.log(a+b)
+export default function resta(a, b) {
+    console.log("la resta es", a - b)
 }
-export function multiplicacion(a, b){
-    console.log(a* b)
+export function suma(a, b) {
+    console.log(a + b)
 }
-export default function resta(a, b){
-    console.log("la resta es",a-b)
+export function multiplicacion(a, b) {
+    console.log(a * b)
+}
+
+export function palindromo(palabra) {
+    // console.log(palabra);
+    // let palindromo = false
+    // for (let i = 0; i < palabra.length; i++) {
+
+    //     if (palabra[i] === palabra[(palabra.length - 1) - i]) {
+    //         console.log(palabra[i]);
+    //         // console.log(palabra.length - i)
+    //         console.log(palabra[(palabra.length - 1) - i])
+    //         palindromo = true;
+    //         console.log("continua")
+    //     } else {
+    //         console.log(palabra[i]);
+    //         // console.log(palabra.length - i)
+    //         console.log(palabra[(palabra.length - 1) - i])
+    //         palindromo = false;
+    //         console.log("no continua")
+    //         break;
+    //     }
+    // }
+
+    // palindromo === true ? console.log("es un palindromo") : console.log("no es palindromo")
+console.log(palabra)
+console.log(palabra.split('').reverse().join('')===palabra)
 }
